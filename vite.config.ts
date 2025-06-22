9f860afcaec5b38077968e8ccf60c9d3533432ad
Nathija Nimantha <nathija.max99@gmail.com>
2025-06-22 15:38:11 +0530

    fix: remove extra quotes from base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "'/my-portfolio-v2/'",
+  base: '/my-portfolio-v2/',
   build: {
     outDir: 'dist',
   },
