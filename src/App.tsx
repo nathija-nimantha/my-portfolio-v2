a1a0d8d6544f6898e924b358c175190d1e80a642
Nathija Nimantha <nathija.max99@gmail.com>
2025-06-22 13:54:45 +0530

    fix: correct import order for BrowserRouter and Routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route, BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import About from "./pages/About"
